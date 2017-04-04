79c46f219b5dc36dc5f0a127b780311c8431573f
Yashwanth <yash.mydam@gmail.com>
2017-04-05 04:07:15 +0530

    Add files via upload

--- Practice/MockupGenerator/js/script.js
+++ Practice/MockupGenerator/js/script.js
@@ -28,9 +28,11 @@ function imageLoader() {
     reader.readAsDataURL(fileInput.files[0]);
 }
 window.dl = function() { 
+    ctxSml.clearRect(0, 0, smallCanvas.width,smallCanvas.height);
     ctx.clearRect(0, 0, bigCanvas.width,bigCanvas.height);
     ctx.drawImage(image,0,0,bigCanvas.width,bigCanvas.height);
-    ctx.drawImage(img,83,61.5,130,55);
+    //ctx.drawImage(img,120,100,smallCanvas.width,smallCanvas.height);
+    ctxSml.drawImage(img,0,0, img.width,img.height,0,0,smallCanvas.width,smallCanvas.height);
     var data1 = bigCanvas.toDataURL('image/png',1.0);
    var imgDLHelper = document.getElementById('imgdlhelper');
    imgDLHelper.setAttribute('href',data1);
