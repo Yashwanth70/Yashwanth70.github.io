6b6bd71166f752d8dc7733367864ff99714d9a40
Yashwanth <yash.mydam@gmail.com>
2017-04-05 03:27:04 +0530

    Add files via upload

--- Practice/MockupGenerator/js/script.js
+++ Practice/MockupGenerator/js/script.js
@@ -1,11 +1,17 @@
-var canvas = document.getElementById('myCanvas');
-var bg = document.getElementById('bg');
-var ctx = canvas.getContext('2d');
-var ctx1=bg.getContext('2d');        
+var bigCanvas = document.querySelector('#c');
+  var ctx= bigCanvas.getContext('2d');
+  var smallCanvas= document.querySelector('#cd');
+  var ctxSml = smallCanvas.getContext('2d');
+  var image=new Image();
+  image.crossOrigin="Anonymous";
+  image.onload = function() {
+    console.log("Loaded Image");
+    ctx.drawImage(image,0,0,bigCanvas.width,bigCanvas.height);
+    //save File
+   // window.open(document.querySelector('#c').toDataURL());
+  }
+  image.src = "imgx2.jpg";
 
-var back= new Image();
- back.src="img/background.jpg";
- ctx1.drawImage(back,0,0,bg.width,bg.height);
 // Trigger the imageLoader function when a file has been selected
 var fileInput = document.getElementById('fileInput');
 fileInput.addEventListener('change', imageLoader, false);
@@ -14,26 +20,18 @@ function imageLoader() {
     reader.onload = function(event) {
         img = new Image();
         img.onload = function(){
-            ctx.drawImage(img,0,0, img.width,img.height,0,0,canvas.width , canvas.height);
-            ctx1.drawImage(img,126,185,bg.width,bg.height);
+          ctx.drawImage(image,0,0,bigCanvas.width,bigCanvas.height);
+            ctxSml.drawImage(img,0,0, img.width,img.height,0,0,smallCanvas.width,smallCanvas.height);
         }
         img.src = reader.result;
 
     }
     reader.readAsDataURL(fileInput.files[0]);
 }
-
-window.dl = function() {
-    // var back= new Image();
-    // var imgu=new Image();
-    // back.src="img/background.jpg";
-    // imgu.src="img/imgx2.jpg";
-    // ctx1.clearRect(0, 0, canvas.width, canvas.height);
-    // ctx1.drawImage(back,0,0,bg.width,bg.height);
-    //ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    ctx1.drawImage(img,83,61.5,130,55);
-    var data1 = bg.toDataURL('image/png',1.0);
+window.dl = function() { 
+    //ctx.drawImage(image,0,0,bigCanvas.width,bigCanvas.height);   
+    ctxSml.drawImage(img,83,61.5,130,55);
+    var data1 = bigCanvas.toDataURL('image/png',1.0);
    var imgDLHelper = document.getElementById('imgdlhelper');
    imgDLHelper.setAttribute('href',data1);
    imgDLHelper.click();
