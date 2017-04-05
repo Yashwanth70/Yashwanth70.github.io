cda59ab07a8e497e4a9d1130d3bc9245ef7f1ddd
Yashwanth <yash.mydam@gmail.com>
2017-04-05 11:17:04 +0530

    Add files via upload

--- Practice/MockupGenerator/js/script.js
+++ Practice/MockupGenerator/js/script.js
@@ -2,15 +2,16 @@ var bigCanvas = document.querySelector('#c');
   var ctx= bigCanvas.getContext('2d');
   var smallCanvas= document.querySelector('#cd');
   var ctxSml = smallCanvas.getContext('2d');
+  var div = document.querySelector('#div');
   var image=new Image();
-  image.crossOrigin="Anonymous";
+  //image.crossOrigin="Anonymous";
   image.onload = function() {
     console.log("Loaded Image");
     ctx.drawImage(image,0,0,bigCanvas.width,bigCanvas.height);
     //save File
    // window.open(document.querySelector('#c').toDataURL());
   }
-  image.src = "imgx2.jpg";
+  image.src = "img/imgx2.jpg";
 
 // Trigger the imageLoader function when a file has been selected
 var fileInput = document.getElementById('fileInput');
@@ -28,7 +29,9 @@ function imageLoader() {
     reader.readAsDataURL(fileInput.files[0]);
 }
 window.dl = function() { 
-    //ctxSml.clearRect(0, 0, smallCanvas.width,smallCanvas.height);
+  var data2 = smallCanvas.toDataURL('image/png',1.0);
+    $("div").css({"background":data2});
+    ctxSml.clearRect(0, 0, smallCanvas.width,smallCanvas.height);  //Dont include in Github
     ctx.clearRect(0, 0, bigCanvas.width,bigCanvas.height);
     ctx.drawImage(image,0,0,bigCanvas.width,bigCanvas.height);
     ctx.drawImage(img,120,100,smallCanvas.width,smallCanvas.height);
